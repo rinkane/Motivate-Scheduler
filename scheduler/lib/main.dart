a8c4846db83b1d2de7abaddd31292d470e7527b0
rinkane <rinkaneisdead@gmail.com>
2022-02-20 18:29:37 +0900

    状態を持ったダイアログを表示できるようにした

--- scheduler/lib/main.dart
+++ scheduler/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -29,12 +29,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  String testText = "test";
+  void showAddScheduleDialog() async {
+    final schedule = await showDialog<String>(
+        context: context,
+        builder: (context) =>
+            AddScheduleDialog(initialMotivation: 0.0, initialScheduleName: ""));
+    if (schedule != null) {
+      setState(() {
+        testText = schedule;
+      });
+    }
   }
 
   @override
@@ -42,13 +47,80 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: CustomScrollView(slivers: [
         SliverAppBar(title: Text("Motivate-Scheduler")),
-        SliverList(delegate: SliverChildListDelegate([]))
+        SliverList(delegate: SliverChildListDelegate([Text(testText)]))
       ]),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: showAddScheduleDialog,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+class AddScheduleDialog extends StatefulWidget {
+  final String initialScheduleName;
+  final double initialMotivation;
+
+  const AddScheduleDialog(
+      {Key? key,
+      required this.initialScheduleName,
+      required this.initialMotivation})
+      : super(key: key);
+
+  @override
+  AddScheduleDialogState createState() => AddScheduleDialogState();
+}
+
+class AddScheduleDialogState extends State<AddScheduleDialog> {
+  late String scheduleName;
+  late double motivation;
+
+  @override
+  void initState() {
+    super.initState();
+    scheduleName = widget.initialScheduleName;
+    motivation = widget.initialMotivation;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        title: Text("スケジュールの追加"),
+        content: Column(children: <Widget>[
+          TextField(
+              decoration: InputDecoration(hintText: "追加したい予定"),
+              onChanged: changeTextField),
+          Slider(
+            value: motivation,
+            max: 100,
+            min: -100,
+            onChanged: changeSlider,
+          )
+        ]),
+        actions: <Widget>[
+          ElevatedButton(
+              child: Text("追加"),
+              onPressed: () {
+                Navigator.pop(context, scheduleName);
+              }),
+          ElevatedButton(
+              child: Text("キャンセル"),
+              onPressed: () {
+                Navigator.pop(context);
+              })
+        ]);
+  }
+
+  void changeSlider(double value) {
+    setState(() {
+      motivation = value.roundToDouble();
+    });
+  }
+
+  void changeTextField(String value) {
+    setState(() {
+      scheduleName = value;
+    });
   }
 }
