85763e0b126dba5112288af4483dc838745b96cc
rinkane <rinkaneisdead@gmail.com>
2022-02-21 22:01:26 +0900

    コード整理

--- scheduler/lib/model/schedule.dart
+++ scheduler/lib/model/schedule.dart
@@ -1,8 +1,10 @@
 class Schedule {
   String name = "";
   double motivation = 0.0;
+
   DateTime startDateTime = DateTime(DateTime.now().year, DateTime.now().month,
       DateTime.now().day, DateTime.now().hour, DateTime.now().minute);
+
   DateTime endDateTime = DateTime(DateTime.now().year, DateTime.now().month,
       DateTime.now().day, DateTime.now().hour, DateTime.now().minute);
 
