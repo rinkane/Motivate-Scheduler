5ad3ef4ce7dd4841d7e0554585e9c30df8ee0c58
rinkane <rinkaneisdead@gmail.com>
2022-02-24 17:44:24 +0900

    flutterfireで生成したオプションを使ってFirebase Authenticationにユーザ登録できるようにした

---
+++ scheduler/lib/view/registUserPage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegistUserPage extends StatefulWidget {
+  const RegistUserPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegistUserPage> createState() => _RegistUserPageState();
+}
+
+class _RegistUserPageState extends State<RegistUserPage> {
+  String mailAddress = "";
+  String password = "";
+  String infoText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "MailAddress",
+                ),
+                onChanged: (String input) {
+                  setState(() {
+                    mailAddress = input;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "Password",
+                ),
+                obscureText: true,
+                onChanged: (String input) {
+                  setState(() {
+                    password = input;
+                  });
+                },
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                child: const Text("登録"),
+                onPressed: () async {
+                  try {
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final UserCredential result =
+                        await auth.createUserWithEmailAndPassword(
+                      email: mailAddress,
+                      password: password,
+                    );
+                    final User user = result.user!;
+                    setState(() {
+                      infoText = "登録成功:${user.email}";
+                    });
+                  } catch (e) {
+                    setState(() {
+                      infoText = "登録失敗:${e.toString()}";
+                    });
+                  }
+                },
+              ),
+              const SizedBox(height: 8),
+              Text(infoText),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
