5d838bf3facb5c04b949bd83d02ae47dd8251005
rinkane <rinkaneisdead@gmail.com>
2022-03-02 11:25:20 +0900

    メソッド分割のリファクタ

--- scheduler/lib/model/schedule.dart
+++ scheduler/lib/model/schedule.dart
@@ -91,13 +91,19 @@ class Schedule {
   String getDoubleBookedWarningText() {
     if (_doubleBookedSchedules.isEmpty) return "";
 
-    String warning = "It's double-booked with ";
+    String warning =
+        "It's double-booked with " + _getDoubleBookedSchedulesText();
+
+    return warning;
+  }
+
+  String _getDoubleBookedSchedulesText() {
+    String warning = "";
     for (var doubleBookingSchedule in _doubleBookedSchedules) {
       warning += '"' + doubleBookingSchedule.name + '"';
       warning += " , ";
     }
-    warning = warning.replaceRange(warning.length - 3, null, '.');
-    return warning;
+    return warning.replaceRange(warning.length - 3, null, '.');
   }
 
   bool isCompleteAt(DateTime now) {
