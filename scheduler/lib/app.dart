de4f2c60831b9bef60ee52c0e60606a00b5ddf0a
rinkane <rinkaneisdead@gmail.com>
2022-03-05 19:40:12 +0900

    スタートページの修正

--- scheduler/lib/app.dart
+++ scheduler/lib/app.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:scheduler/ui/dialogs/schedule_setting_dialog.dart';
-import 'package:scheduler/ui/widgets/slider/labeled_slider.dart';
-
 import 'ui/pages/login_page.dart';
 
 const String appName = "Motivate Scheduler";
@@ -16,8 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      //home: const LoginPage(),
-      home: ScheduleSettingDialog(initialMethod: ScheduleSettingMethod.add),
+      home: const LoginPage(),
     );
   }
 }
